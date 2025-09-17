572e79d379ded3584231483cb7b4ebfcf533b273
Ahsan Hasby <mahahsanhasby@gmail.com>
2025-09-17 11:53:41 +0600

    Sparse Matrix

--- Sparse_Matrix/Sparse_Matrix.cpp
+++ Sparse_Matrix/Sparse_Matrix.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 int main() {
     int row = 3, column = 3;
-    int TwoDimension[row][column] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    
+    int TwoDimension[row][column] = {
+        {1, 0, 0}, 
+        {4, 5, 0}, 
+        {7, 8, 9}};
+    int OneDimension[row*column];
+
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j <= min(i, j); j++) {
+            OneDimension[(i*column) + j] = TwoDimension[i][j];
+        }
+    }
+
+    for(auto i : OneDimension) cout << i << " ";
+    cout << endl;
+
     return 0;
 }
