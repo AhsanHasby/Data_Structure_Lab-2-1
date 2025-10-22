d630ea4b97ce2784d922e27963cab51ed6ea5322
Ahsan Hasby <mahahsanhasby@gmail.com>
2025-10-22 11:28:20 +0600

    Added Linked List

--- Linked List/Create_Linked_List.c
+++ Linked List/Create_Linked_List.c
@@ -15,10 +15,11 @@ struct Node* create_node(int value) {
 
 int main() {
     struct Node *head = NULL, *temp = NULL, *new_node = NULL;
-    int i, value;
-    printf("Enter 5 integer: \n");
+    int i, n, value;
+    printf("How many number you want to insert: \n");
+    scanf("%d", &n);
     
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d", &value);
         new_node = create_node(value);
         if (head == NULL) {
@@ -30,6 +31,15 @@ int main() {
             temp = temp -> next;
         }
     }
+
+    printf("Display linked list nodes: \n");
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp -> data);
+        temp = temp -> next;
+        
+    }
+    printf("\n");
 
     return 0;
 }
